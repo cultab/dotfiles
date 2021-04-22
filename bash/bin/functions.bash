6fecb38ca739fff7769ab1a8d444bfddd98e0584
cultab <rroarck@gmail.com>
2021-04-22 11:00:36 +0300

    bash aliases function
    also bg-choose changes

--- bash/bin/functions.bash
+++ bash/bin/functions.bash
@@ -55,19 +55,39 @@ mkcd () {
     mkdir "$1" && cd "$1"
 }
 
+project () {
+    mkdir "$1"
+    echo "Created project folder $1.."
+    cp -r ~/Documents/template/* "./$1"
+    echo "Copied template files.."
+    cd "$1"
+    echo "cd'ed into $1"
+    git init
+    echo "Initialized git repo"
+    git add -- *
+    echo "Added already existing files to repo"
+    git commit -m 'project init'
+    echo "Commited initial files to project repo"
+}
+
 rmd_template () {
-    cp -r ~/Documents/template/* ./
+    project
 }
 
 enable_more_completions () {
-    # pandoc completion
+    # pandoc
     if [ -x "$(command -v pandoc)" ]; then
         eval "$(pandoc --bash-completion)"
     fi
-    # flutter completion
+    # flutter
     if [ -x "$(command -v flutter)" ]; then
         eval "$(flutter bash-completion)"
     fi
+    # pipenv
+    if [ -x "$(command -v pipenv)" ]; then
+        eval "$(pipenv --completion)"
+    fi
+
 }
 
 # nnnvim ()
