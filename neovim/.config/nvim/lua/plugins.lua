cc78e13c4de6304dd6e661be1b53c4c411d7a4a1
cultab <rroarck@gmail.com>
2021-12-19 04:42:22 +0200

    nvim: add dressing.nvim

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -136,6 +136,7 @@ return require('packer').startup(function(use)
         end
     }--}}}
     use { 'lukas-reineke/indent-blankline.nvim' }
+    use {'stevearc/dressing.nvim'}
     use { 'nvim-lualine/lualine.nvim',--{{{
         requires = {'kyazdani42/nvim-web-devicons', opt = true}
     }--}}}
