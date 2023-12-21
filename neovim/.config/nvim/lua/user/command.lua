b5d5c4fad6738fca46cc425c41b672a883efa09c
cultab <rroarck@gmail.com>
2023-12-21 19:34:52 +0200

    neovim: command.lua wezterm support

--- neovim/.config/nvim/lua/user/command.lua
+++ neovim/.config/nvim/lua/user/command.lua
@@ -1,20 +1,65 @@
 local M = {}
-local icons = require'user.icons'
+local icons = require 'user.icons'
 LastCommand = nil
+
+local function ToggleTerm(cmd)
+    require 'toggleterm'.exec(cmd)
+end
+
+local function Tmux(cmd)
+    vim.notify("user/command: the tmux backend is still unimplemented")
+end
+
+local function weztermCli(subcmd)
+    local cli = "wezterm cli "
+    local pipe = io.popen(cli .. subcmd)
+    if not pipe then
+        return "", "failed to run wezterm cli subcmd: " .. subcmd
+    end
+    local ret = pipe:read()
+    pipe:close()
+    return ret, nil
+end
+
+local function weztermRun(cmd, pane_id)
+    local _, err = weztermCli("send-text --no-paste --pane-id " .. pane_id .. " -- '" .. cmd .. "\n'")
+    if err ~= nil then
+        vim.notify("user/command failed to run command: " .. err)
+        return
+    end
+end
+
+local function Wezterm(cmd)
+    local pane, err = weztermCli("get-pane-direction right")
+    if err ~= nil then
+        vim.notify("user/command: " .. err)
+    end
+    if not pane then
+        pane = weztermCli("split-pane --right")
+        _, err = weztermCli("activate-pane-direction left")
+        if err ~= nil then
+            vim.notify(err)
+        end
+    end
+    weztermRun(cmd, pane)
+end
+
+-- local backend = ToggleTerm
+local backend = Wezterm
 
 
 M.run_command = function()
-    vim.ui.input({ prompt = icons.misc.term .. "cmd: ", completion = 'shellcmd' }, function (command)
+    vim.ui.input({ prompt = icons.misc.term .. "cmd: ", completion = 'shellcmd' }, function(command)
         if command then
             LastCommand = command
-            vim.cmd(":1TermExec cmd='" .. command .. "'")
+            backend(command)
         end
     end)
 end
 
 M.run_last_command = function()
     if LastCommand then
-        vim.cmd(":1TermExec cmd='" .. LastCommand .. "'")
+        backend(LastCommand)
     else
         vim.notify(icons.misc.term .. "No command to repeat", nil, { title = "mappings.lua" })
     end
@@ -29,9 +74,11 @@ M.run_current_file = function()
         command = "make render"
     elseif filename == "report.qmd" then
         command = "quarto render " .. filename .. " --to pdf"
+    elseif string.gmatch(filename, ".*\\.py") then
+        command = "python3 " .. filename
     end
 
-    vim.cmd(":1TermExec cmd='" .. command .. "'")
+    backend(command)
     LastCommand = command
 end
 
