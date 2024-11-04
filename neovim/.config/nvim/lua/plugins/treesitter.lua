e4fa87c78d1514ba3526f8814f1cd3bd2894e015
cultab <rroarck@gmail.com>
2024-11-05 00:18:38 +0200

    nvim: install TreeSitter parsers as languages are encountered
    stop lazy loading cmp modules
    add colorscheme back into lazy's config

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
 		config = function()
 			require('nvim-treesitter.configs').setup {
 				-- ensure_installed = "all", -- one of "all"  or a list of languages
+				auto_install = true,
 				highlight = { enable = true },
 				incremental_selection = { enable = true },
 				textobjects = {
